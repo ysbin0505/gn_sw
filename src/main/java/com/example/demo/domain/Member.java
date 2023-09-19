3711595ade0b973e038f4cfb480715919e95e952
ysbin0505 <chfhddl515@gmail.com>
2023-09-19 14:13:21 +0900

    ??

--- src/main/java/com/example/demo/domain/Member.java
+++ src/main/java/com/example/demo/domain/Member.java
@@ -1,23 +1,15 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 @Data
 @Entity
-@Getter
-@Setter
 public class Member {
 
-    @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
+    @Id
     private Long id;
     private String nickname;
     private String email;
-
+//
 }
