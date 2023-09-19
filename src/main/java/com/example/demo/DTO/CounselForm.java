d209dca1450566b4989d5a02efb143ef901adf10
ysbin0505 <chfhddl515@gmail.com>
2023-09-19 17:37:10 +0900

    상담 api 완료

---
+++ src/main/java/com/example/demo/DTO/CounselForm.java
@@ -0,0 +1,13 @@
+package com.example.demo.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CounselForm {
+  private String email;
+  private String context;
+
+
+}
--- src/main/java/com/example/demo/domain/Counsel.java
+++ src/main/java/com/example/demo/domain/Counsel.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Counsel {
   @Id @GeneratedValue
   @Column(name = "COUNSEL_ID")
