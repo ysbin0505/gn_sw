b8c828fdeb1e36b36cbfae4c715e862291c1f65b
ysbin0505 <chfhddl515@gmail.com>
2023-09-21 19:25:54 +0900

    관광지 데이터 띄우기 완료 비로그인이 뭐 하려하면 에러 메시지 뜨게 완료 상담 페이지 따로 생성 및 띄우기 완료

--- src/main/java/com/example/demo/controller/CounselController.java
+++ src/main/java/com/example/demo/controller/CounselController.java
@@ -1,30 +1,44 @@
 package com.example.demo.controller;
 
 import com.example.demo.DTO.CounselForm;
+import com.example.demo.domain.Counsel;
 import com.example.demo.service.CounselService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+import java.util.List;
+
 @Controller
-@RequiredArgsConstructor
 public class CounselController {
   private final CounselService counselService;
 
-  @GetMapping(value = "/counsels/new")
-  public String createForm(Model model) {
-    model.addAttribute("counselForm", new CounselForm());
-    return "redirect:/index.html";
+  @Autowired
+  public CounselController(CounselService counselService) {
+    this.counselService = counselService;
   }
-
 
   @PostMapping(value = "/counsels/new")
   public String create(@Valid CounselForm form, BindingResult result) {
     counselService.saveCounsel(form);
-    return "redirect:/index.html";
+    return "redirect:/counsels";
+  }
+
+  @GetMapping("/counsels")
+  public String showAllCounsels(Model model) {
+    List<Counsel> allCounsels = counselService.findAll();
+    model.addAttribute("counsels", allCounsels);
+    return "counsels";
+  }
+
+  @GetMapping("/home")
+  public String home(Model model) {
+    // 여기에 모델에 데이터를 추가하거나 다른 로직을 추가할 수 있습니다.
+    return "home"; // "home.html" 템플릿을 찾아 렌더링합니다.
   }
 }
--- src/main/java/com/example/demo/service/CounselService.java
+++ src/main/java/com/example/demo/service/CounselService.java
@@ -3,17 +3,26 @@ package com.example.demo.service;
 import com.example.demo.DTO.CounselForm;
 import com.example.demo.domain.Counsel;
 import com.example.demo.repository.CounselRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class CounselService {
+
   private final CounselRepository counselRepository;
+
+  @Autowired
+  public CounselService(CounselRepository counselRepository) {
+    this.counselRepository = counselRepository;
+  }
+
+  // 모든 Counsel 데이터를 가져오는 메서드
+  public List<Counsel> findAll() {
+    return counselRepository.findAll();
+  }
 
   @Transactional
   public void saveCounsel(CounselForm counselForm) {
@@ -25,32 +34,7 @@ public class CounselService {
 
   @Transactional //변경 + readonly false
   public Long join(Counsel counsel){
-    validateDuplicateMember(counsel);    //중복 회원 검증
     counselRepository.save(counsel);
     return counsel.getId();   //em.persist 아직 db에 들어간 시점이 아니라도 값을 넣어줌
-  }
-
-  private void validateDuplicateMember(Counsel counsel) {
-    //EXCEPTION
-    List<Counsel> findCounsels = counselRepository.findByemail(counsel.getEmail()); //멀티쓰레드 예방으로 DB에서 member의 name을 unique로 지정
-    if (!findCounsels.isEmpty()){
-      throw new IllegalStateException("이미 존재하는 회원입니다.");   //member수를 카운트해서 0보다 크다 라고 하면 더 최적화 되긴함
-    }
-  }
-
-  public Counsel findOne(Long counselId){
-    return counselRepository.findOne(counselId);
-  }
-
-  public List<Counsel> findCounsels(){
-    return counselRepository.findAll(); //전체 조회
-  }
-
-
-  @Transactional
-  public void update(Long id, String email, String context){
-    Counsel counsel = counselRepository.findOne(id);
-    counsel.setEmail(email);
-    counsel.setContext(context);
   }
 }
